ff5febcd1d44a0a3e8f9f5a92b08d02a0ca6551b
Niko Hakala <niko_hakala_94@hotmail.com>
2019-10-02 13:37:59 +0300

    add new functionality

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,20 +1,50 @@
 import React, {useState, useEffect} from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-
-
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Addcar from './Addcar'
 
 const Carlist = () => {
     const [cars, setCars] = useState([]);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         fetchCars();
     }, [])
 
+    const handleClose = (event, reason) => {
+        setOpen(false);
+    };
+
     const fetchCars = () => {
         fetch('https://carstockrest.herokuapp.com/cars')
         .then(resp => resp.json())
         .then(respData => setCars(respData._embedded.cars))
+        .catch(err => console.error(err))
+    }
+
+    const deleteCar = (link) => {
+        if (window.confirm('Are you sure?')){
+            fetch(link, {method: 'DELETE'})
+            .then(res => fetchCars())
+            .then(res => setOpen(true))
+            .catch(err => console.error(err))
+        }
+    }
+
+    const saveCar = (newCar) => {
+        fetch('https://carstockrest.herokuapp.com/cars', 
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newCar)
+            }
+        )
+        .then(res => fetchCars())
+        .catch(err => console.error(err))
     }
 
     const columns = [
@@ -42,11 +72,19 @@ const Carlist = () => {
             Header: 'Price (€)',
             accessor: 'price'
         },
+        {
+            accessor: '_links.self.href',
+            filterable: false,
+            sortable: false,
+            Cell: ({value}) => <Button color="secondary" size="small" onClick={() => deleteCar(value)}>Delete</Button>
+        },
     ]
 
     return (
         <div>
+            <Addcar saveCar={saveCar}/>
             <ReactTable filterable={true} columns={columns} data={cars}/>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} message='Car Deleted'/>
         </div>
     );
 };
